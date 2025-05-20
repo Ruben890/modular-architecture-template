4b29cc80cdbb6fb31079aea0bb3db52a991229b0
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-05-20 16:14:20 -0400

    ,

---
+++ Modules/Mpdules.User/Domain/Interfaces/IRepository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Mpdules.User.Domain.Interfaces.IRepository
+{
+    public interface IUserRepository
+    {
+        public void AddUser(User user);
+        public void DeleteUser(User user);
+        public void UpdateUser(User user);
+    }
+}
