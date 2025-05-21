6c1eca862ace3df9f29fb69262dbc4af96b7b968
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-05-20 20:35:38 -0400

    Corrección de espacios de nombres en GetUserByEmailOrUserNameQuery.cs
    Se cambió el espacio de nombres de `Shared.Messages.Queries` a `Shared.Messages.Querys`, corrigiendo un error tipográfico. También se modificó el espacio de nombres de la clase de `Module.User.Application.Handlers.Queries` a `Module.User.Application.Handlers.Query`. La lógica del manejador `GetUserByEmailOrUserNameHandler` y su método `Handle` permanecen sin cambios.

---
+++ Modules/Module.User/Application/Handdle/Queries/GetUserByEmailOrUserNameQuery.cs
@@ -0,0 +1,23 @@
+﻿using Marten;
+using Shared.Messages.Queries;
+using UserDto = Shared.DTO.Dtos.User;
+
+
+namespace Module.User.Application.Handlers.Queries
+{
+    public class GetUserByEmailOrUserNameHandler
+    {
+        private readonly IDocumentSession _session;
+
+        public GetUserByEmailOrUserNameHandler(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public Task<UserDto?> Handle(GetUserByEmailOrUserNameQuery query, CancellationToken ct)
+        {
+            return _session.Query<UserDto>()
+                .FirstOrDefaultAsync(x => x.Email == query.Email || x.UserName == query.UserName, ct);
+        }
+    }
+}
---
+++ Modules/Module.User/Application/Handdle/Query/GetUserByEmailOrUserNameQuery.cs
@@ -0,0 +1,23 @@
+﻿using Marten;
+using Shared.Messages.Querys;
+using UserDto = Shared.DTO.Dtos.User;
+
+
+namespace Module.User.Application.Handlers.Query
+{
+    public class GetUserByEmailOrUserNameHandler
+    {
+        private readonly IDocumentSession _session;
+
+        public GetUserByEmailOrUserNameHandler(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public Task<UserDto?> Handle(GetUserByEmailOrUserNameQuery query, CancellationToken ct)
+        {
+            return _session.Query<UserDto>()
+                .FirstOrDefaultAsync(x => x.Email == query.Email || x.UserName == query.UserName, ct);
+        }
+    }
+}
