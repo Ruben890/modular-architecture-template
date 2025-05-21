e78d5241e7d7c39ea044079be495adcfb2c1aa7d
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-05-21 10:23:56 -0400

    Añadir soporte para Wolverine y mensajes compartidos
    Se ha incorporado la biblioteca `Wolverine` en `AuthService.cs`, implementando un bus de mensajes (`IMessageBus`) con un nuevo campo privado `_bus` y un constructor correspondiente.
    
    Además, se ha añadido una referencia al proyecto `Shared.Messages` en `Module.Auth.csproj` para facilitar el uso de mensajes compartidos en la aplicación de autenticación.

--- Modules/Module.Auth/Application/Services/AuthService.cs
+++ Modules/Module.Auth/Application/Services/AuthService.cs
@@ -1,10 +1,17 @@
 ﻿using Module.Auth.Domain.Interfaces;
+using Wolverine;
 
 namespace Module.Auth.Application.Services
 {
     public class AuthService : IAuthService
     {
 
+        private readonly IMessageBus _bus;
+
+        public AuthService(IMessageBus bus)
+        {
+            _bus = bus;
+        }
 
 
 
