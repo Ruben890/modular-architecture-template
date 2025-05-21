3d30a02f2ac9c7cefb2a6c15e62fbb504ce6a7e4
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-05-21 16:45:11 -0400

    .

--- Modules/Module.User/Application/Handdle/Queries/GetUserByEmailOrUserNameQuery.cs
+++ Modules/Module.User/Application/Handdle/Queries/GetUserByEmailOrUserNameQuery.cs
@@ -1,4 +1,5 @@
 ﻿using Module.User.Domain.Interfaces.IRepository;
+using Shared.Messages.Queries;
 using Wolverine.Attributes;
 using UserDto = Shared.DTO.Dtos.User;
 
@@ -6,16 +7,16 @@ using UserDto = Shared.DTO.Dtos.User;
 namespace Module.User.Application.Handlers.Queries
 {
     [WolverineHandler]
-    public class GetUserByEmailOrUserNameHandler
+    public class GetUserByEmailOrUserNameQueries
     {
         private readonly IUserRepository _userRepository;
 
-        public GetUserByEmailOrUserNameHandler(IUserRepository userRepository)
+        public GetUserByEmailOrUserNameQueries(IUserRepository userRepository)
         {
             _userRepository = userRepository;
         }
 
-        public Task<UserDto?> Handle(Shared.Messages.Queries.GetUserByEmailOrUserNameHandler query, CancellationToken ct)
+        public Task<UserDto?> Handle(GetUserByEmailOrUserNameHandler query, CancellationToken ct)
         {
             return _userRepository.GetUserByEmailOrUserName(query.Email, query.UserName);
         }
