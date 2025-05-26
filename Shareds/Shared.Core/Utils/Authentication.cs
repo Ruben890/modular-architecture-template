edaf6a7f209189bcdfe105925d1ad48c41f5f7f0
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-05-26 14:40:24 -0400

    Añadir clase Authentication para gestión de JWT
    Se ha implementado la clase estática `Authentication` que proporciona utilidades para la autenticación y generación de tokens JWT. Se han inicializado las propiedades `issuer`, `audience` y `key` desde la configuración del entorno.
    
    El método `GenerateClaims` ha sido modificado para mejorar el manejo de valores nulos y se ha cambiado la lógica de generación de claims. Además, se ha renombrado el método `GenerateToken` a `GenerateJwtToken` y se ha añadido `GenerateRefreshToken` para crear un token de refresco seguro.
    
    Se han eliminado comentarios en español y se han realizado ajustes en la lógica de generación de tokens.

--- Shareds/Shared.Core/Utils/Authentication.cs
+++ Shareds/Shared.Core/Utils/Authentication.cs
@@ -9,18 +9,13 @@ using System.Text;
 
 namespace Shared.Core.Utils
 {
-    /// <summary>
-    /// Clase estática que proporciona utilidades para la autenticación y generación de tokens JWT.
-    /// </summary>
+
     public static class Authentication
     {
         private static readonly string issuer;
         private static readonly string audience;
         private static readonly string key;
 
-        /// <summary>
-        /// Constructor estático para inicializar los valores de configuración.
-        /// </summary>
         static Authentication()
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
@@ -35,42 +30,29 @@ namespace Shared.Core.Utils
             key = GetConfigurationValue(configuration, "Jwt:Key");
         }
 
-        /// <summary>
-        /// Obtiene el valor de una clave de configuración y lanza una excepción si el valor es nulo o vacío.
-        /// </summary>
-        /// <param name="configuration">La configuración de la cual obtener el valor.</param>
-        /// <param name="key">La clave de configuración.</param>
-        /// <returns>El valor de configuración.</returns>
-        /// <exception cref="InvalidOperationException">Se lanza si el valor de configuración es nulo o vacío.</exception>
         private static string GetConfigurationValue(IConfiguration configuration, string key)
         {
             return configuration[key] ?? throw new InvalidOperationException($"The value '{key}' in the configuration cannot be null or empty.");
         }
 
-        /// <summary>
-        /// Genera una lista de <see cref="Claim"/> a partir de las propiedades públicas de un objeto.
-        /// </summary>
-        /// <param name="obj">El objeto del cual generar los claims.</param>
-        /// <returns>Una lista de claims.</returns>
         private static IEnumerable<Claim> GenerateClaims(object data)
         {
             var claims = new List<Claim>();
 
             if (data != null)
             {
-                // Si es una sola propiedad, agregamos una sola claim
-                if (data.GetType().IsValueType || data.GetType() == typeof(string))
+                if (data.GetType().IsValueType || data is string)
                 {
-                    claims.Add(new Claim(data.GetType().Name, data.ToString()!));
+                    claims.Add(new Claim("Value", data.ToString()!));
                 }
-                else // Si es un objeto completo, agregamos todas las propiedades como claims
+                else
                 {
                     var properties = data.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
                     foreach (var property in properties)
                     {
                         var value = property.GetValue(data)?.ToString();
-                        if (value != null)
+                        if (!string.IsNullOrEmpty(value))
                         {
                             claims.Add(new Claim(property.Name, value));
                         }
@@ -81,48 +63,15 @@ namespace Shared.Core.Utils
             return claims;
         }
 
-        /// <summary>
-        /// Genera un token JWT basado en los claims generados a partir de un objeto y una fecha de expiración.
-        /// </summary>
-        /// <param name="data">El objeto del cual generar los claims.</param>
-        /// <param name="expires">La fecha y hora de expiración del token.</param>
-        /// <returns>El token JWT como una cadena.</returns>
         public static Token GenerateToken(object data, DateTime expires)
         {
             var claims = GenerateClaims(data);
-            var token = GenerateToken(claims, expires);
-
+            var accessToken = GenerateJwtToken(claims, expires);
             var refreshToken = GenerateRefreshToken();
-
-            return new Token(token, refreshToken);
+            return new Token(accessToken, refreshToken);
         }
 
-        /// <summary>
-        /// Generates a secure random refresh token.
-        /// </summary>
-        /// <returns>A base64 encoded string representing the refresh token.</returns>
-        /// <remarks>
-        /// This method creates a secure random token by generating 32 bytes of random data 
-        /// and then encoding it using Base64. The RandomNumberGenerator class is used to 
-        /// ensure cryptographic strength of the random numbers.
-        /// </remarks>
-        private static string GenerateRefreshToken()
-        {
-            var randomNumber = new byte[32];
-            using (var rng = RandomNumberGenerator.Create())
-            {
-                rng.GetBytes(randomNumber);
-                return Convert.ToBase64String(randomNumber);
-            }
-        }
-
-        /// <summary>
-        /// Genera un token JWT basado en una lista de claims y una fecha de expiración.
-        /// </summary>
-        /// <param name="claims">La lista de claims para incluir en el token.</param>
-        /// <param name="expires">La fecha y hora de expiración del token.</param>
-        /// <returns>El token JWT como una cadena.</returns>
-        private static string GenerateToken(IEnumerable<Claim> claims, DateTime expires)
+        private static string GenerateJwtToken(IEnumerable<Claim> claims, DateTime expires)
         {
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
@@ -136,6 +85,14 @@ namespace Shared.Core.Utils
             );
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
         }
     }
 }
