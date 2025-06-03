ff9c1ba707db6b0d772d1edcd3cfa5b4c5ea24db
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-06-03 18:55:09 -0400

    Añadir consulta para obtener usuario por ID
    Se han añadido nuevas importaciones en `GetUserByIdQueries.cs` y se ha definido el espacio de nombres `Module.User.Application.Queries`.
    
    Se ha implementado la clase `GetUserByIdQueries` con un constructor que inicializa el repositorio de usuarios (`IUserRepository`) y un método `Handle` que obtiene un usuario por su ID y lo adapta a un objeto `UserDto` utilizando `Mapster`.

---
+++ Modules/Module.User/Application/Queries/GetUserByIdQueries.cs
@@ -0,0 +1,27 @@
+﻿using Mapster;
+using Module.User.Domain.Interfaces.IRepository;
+using Shared.Core.Attributes;
+using Shared.Messages.Queries;
+using Wolverine.Attributes;
+using UserDto = Shared.DTO.Dtos.User;
+
+namespace Module.User.Application.Queries
+{
+    [WolverineHandler]
+    [WModuleHandler]
+    public class GetUserByIdQueries
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByIdQueries(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDto?> Handle(GetUserById query, CancellationToken ct)
+        {
+            var userEntity = await _userRepository.GetUserById(query.UserId);
+            return userEntity!.Adapt<UserDto>();
+        }
+    }
+}
