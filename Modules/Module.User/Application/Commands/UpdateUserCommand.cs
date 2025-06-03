70bb571718e97af1813ea8e2826507c4ad7eaf4e
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-06-03 18:35:51 -0400

    Añadir clases para actualizar y obtener usuarios
    Se han implementado las clases `UpdateUserCommand` y `GetUserByEmailOrUserNameQueries`.
    Ambas utilizan Mapster para la adaptación de entidades y DTOs, y están diseñadas para interactuar
    con el repositorio de usuarios a través de `IUserRepository`.
    Se han añadido atributos de Wolverine para manejar las solicitudes de manera asíncrona.

---
+++ Modules/Module.User/Application/Commands/UpdateUserCommand.cs
@@ -0,0 +1,26 @@
+﻿using Mapster;
+using Module.User.Domain.Entity;
+using Module.User.Domain.Interfaces.IRepository;
+using Shared.Core.Attributes;
+using Shared.Messages.Commads;
+using Wolverine.Attributes;
+
+namespace Module.User.Application.Commands.User
+{
+    [WolverineHandler]
+    [WModuleHandler]
+    public class UpdateUserCommand
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UpdateUserCommand(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void Handle(UpdateUser command)
+        {
+            _userRepository.UpdateUser(command.User.Adapt<UserEntity>());
+        }
+    }
+}
