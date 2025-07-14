df9311a05912c78c86b2de6677d799dd6300de4d
Darlin Ruben Nina C. <78616515+Ruben890@users.noreply.github.com>
2025-07-14 17:36:34 -0400

    .

--- Shareds/Shared.Core/RepositoryBase.cs
+++ Shareds/Shared.Core/RepositoryBase.cs
@@ -21,14 +21,20 @@ namespace Shared.Core
             trackChanges
                 ? Context.Set<TEntity>()
                 : Context.Set<TEntity>().AsNoTracking();
+
         public IQueryable<TEntity> FindByCondition(Expression<Func<TEntity, bool>> expression, bool trackChanges) =>
             trackChanges
                 ? Context.Set<TEntity>().Where(expression)
                 : Context.Set<TEntity>().Where(expression).AsNoTracking();
+
         public async Task Create(TEntity entity) => await Context.Set<TEntity>().AddAsync(entity);
+
         public void Update(TEntity entity) => Context.Set<TEntity>().Update(entity);
+
         public void Delete(TEntity entity) => Context.Set<TEntity>().Remove(entity);
+
         public void RemoveRange(IEnumerable<TEntity> entities) => Context.Set<TEntity>().RemoveRange(entities);
+
         public void UpdateRange(IEnumerable<TEntity> entities) => Context.Set<TEntity>().UpdateRange(entities);
         public async Task CreateRange(IEnumerable<TEntity> entities) => await Context.Set<TEntity>().AddRangeAsync(entities);
 
